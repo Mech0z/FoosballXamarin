859425a0d4dc2d8cb942910b06ef2caaccf3c35e
Mads <Mads>
2018-09-04 22:20:13 +0200

    Mvvm and IsBusy button on landingpage

---
+++ FoosballXamarin/FoosballXamarin/ViewModels/UrlLandingViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using FoosballXamarin.Services;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace FoosballXamarin.ViewModels
+{
+    public class UrlLandingViewModel : BaseViewModel
+    {
+        bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set => SetProperty(ref _isBusy, value);
+        }
+
+        private string _apiEntry;
+        public string ApiEntry
+        {
+            get => _apiEntry;
+            set => SetProperty(ref _apiEntry, value);
+        }
+
+        private string _messageLabelText;
+        public string MessageLabelText
+        {
+            get => _messageLabelText;
+            set => SetProperty(ref _messageLabelText, value);
+        }
+
+        public ICommand SubmitApiUrlCommand { get; set; }
+        public ICommand OpenGitHubLinkCommand { get; set; }
+        public ILeaderboardService LeaderboardService => DependencyService.Get<ILeaderboardService>();
+
+        public UrlLandingViewModel()
+        {
+            OpenGitHubLinkCommand = new Command(() => Device.OpenUri(new Uri("https://github.com/Mech0z/Foosball")));
+
+            SubmitApiUrlCommand = new Command(async () =>
+            {
+                Preferences.Set("ApiUrlSettings", ApiEntry);
+                IsBusy = true;
+                var isValid = await CanPingLeaderboard();
+                if (isValid)
+                {
+                    MessagingCenter.Send(this, "ApiPingSuccess");
+                }
+                else
+                {
+                    MessageLabelText = "Could not ping api successfully, remember http(s):// in the front and /api/ in the end";
+                }
+            });
+        }
+
+        private async Task<bool> CanPingLeaderboard()
+        {
+            try
+            {
+                var data = await LeaderboardService.GetDataAsync();
+                IsBusy = false;
+                return data != null;
+            }
+            catch (Exception e)
+            {
+                Preferences.Remove("ApiUrlSettings");
+                IsBusy = false;
+                return false;
+            }
+        }
+    }
+}
--- FoosballXamarin/FoosballXamarin/Views/UrlLandingPage.xaml.cs
+++ FoosballXamarin/FoosballXamarin/Views/UrlLandingPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
-using FoosballXamarin.Services;
+using FoosballXamarin.ViewModels;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -10,47 +10,16 @@ namespace FoosballXamarin.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UrlLandingPage
     {
-        //private readonly UrlLandingPageViewModel _viewModel;
-        public ILeaderboardService LeaderboardService => DependencyService.Get<ILeaderboardService>();
+        private readonly UrlLandingViewModel _viewModel;
 
         public UrlLandingPage()
         {
             InitializeComponent();
-        }
 
-        private async Task Button_OnClicked(object sender, EventArgs e)
-        {
-            var apiUrl = UrlEntry.Text;
+            _viewModel = new UrlLandingViewModel();
 
-            Preferences.Set("ApiUrlSettings", apiUrl);
-            var isValid = await CanPingLeaderboard();
-            if (isValid)
-            {
-                ((App) Application.Current).MainPage = new MainPage();
-            }
-            else
-            {
-                MessageLabel.Text = "Login failed";
-            }
-        }
-
-        private async Task<bool> CanPingLeaderboard()
-        {
-            try
-            {
-                var data = await LeaderboardService.GetDataAsync();
-                return data != null;
-            }
-            catch (Exception e)
-            {
-                Preferences.Remove("ApiUrlSettings");
-                return false;
-            }
-        }
-
-        private void Button_GitHubLinkClicked(object sender, EventArgs e)
-        {
-            Device.OpenUri(new Uri("https://github.com/Mech0z/Foosball"));
+            MessagingCenter.Subscribe<UrlLandingViewModel>(this, "ApiPingSuccess",
+                sender => { ((App)Application.Current).MainPage = new MainPage(); });
         }
     }
 }
